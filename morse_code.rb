116aaf13de2dd37277e8d42abd7df96676538b26
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-22 19:24:28 +0000

    Fix linter errors

--- morse_code.rb
+++ morse_code.rb
@@ -1,52 +1,52 @@
-$dict =
-  {
-    '.-' => 'a',
-    '-...' => 'b',
-    '-.-.' => 'c',
-    '-..' => 'd',
-    '.' => 'e',
-    '..-.' => 'f',
-    '--.' => 'g',
-    '....' => 'h',
-    '..' => 'i',
-    '.---' => 'j',
-    '-.-' => 'k',
-    '.-..' => 'l',
-    '--' => 'm',
-    '-.' => 'n',
-    '---' => 'o',
-    '.--.' => 'p',
-    '--.-' => 'q',
-    '.-.' => 'r',
-    '...' => 's',
-    '-' => 't',
-    '..-' => 'u',
-    '...-' => 'v',
-    '.--' => 'w',
-    '-..-' => 'x',
-    '-.--' => 'y',
-    '--..' => 'z'
-  }
-
 def decode_char(morse_code)
-  print $dict[morse_code].upcase;
+  dict =
+    {
+      '.-' => 'a',
+      '-...' => 'b',
+      '-.-.' => 'c',
+      '-..' => 'd',
+      '.' => 'e',
+      '..-.' => 'f',
+      '--.' => 'g',
+      '....' => 'h',
+      '..' => 'i',
+      '.---' => 'j',
+      '-.-' => 'k',
+      '.-..' => 'l',
+      '--' => 'm',
+      '-.' => 'n',
+      '---' => 'o',
+      '.--.' => 'p',
+      '--.-' => 'q',
+      '.-.' => 'r',
+      '...' => 's',
+      '-' => 't',
+      '..-' => 'u',
+      '...-' => 'v',
+      '.--' => 'w',
+      '-..-' => 'x',
+      '-.--' => 'y',
+      '--..' => 'z'
+    }
+
+  print dict[morse_code].upcase
 end
 
-decode_char(".-")
+decode_char('.-')
 
 def decode_word(morse_code)
   word = morse_code.split(' ')
     .map { |char| decode_char(char) }
     .join()
-  print word + ' '
+  print "#{word} "
 end
 
-decode_word("-- -.--")
+decode_word('-- -.--')
 
 def decode(morse_code)
   sentence = morse_code.split('  ')
     .map { |word| decode_word(word) }
-    .join();
+    .join()
   print sentence
 end
 
