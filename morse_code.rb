fe46622f0d20c44303f164ff7664a10886f28f60
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-22 18:50:04 +0000

    Add decode_char and decode_word methods

--- morse_code.rb
+++ morse_code.rb
@@ -1,3 +1,43 @@
-def DecodeMorseCode(str) 
-  return str;
+$dict =
+  {
+    '.-' => 'a',
+    '-...' => 'b',
+    '-.-.' => 'c',
+    '-..' => 'd',
+    '.' => 'e',
+    '..-.' => 'f',
+    '--.' => 'g',
+    '....' => 'h',
+    '..' => 'i',
+    '.---' => 'j',
+    '-.-' => 'k',
+    '.-..' => 'l',
+    '--' => 'm',
+    '-.' => 'n',
+    '---' => 'o',
+    '.--.' => 'p',
+    '--.-' => 'q',
+    '.-.' => 'r',
+    '...' => 's',
+    '-' => 't',
+    '..-' => 'u',
+    '...-' => 'v',
+    '.--' => 'w',
+    '-..-' => 'x',
+    '-.--' => 'y',
+    '--..' => 'z'
+  }
+
+def decode_char(morseCode)
+  print $dict[morseCode].upcase;
 end
+
+# decode_char("..-");
+
+def decode_word(morseCode)
+  word = morseCode.split(' ').map { |char| decode_char(char) }.join()
+  print word + ' '
+end
+
+# decode_word("-- -.--")
+
